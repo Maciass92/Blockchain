d99ba4e8421a966f8029dea003a29083c1b9d1ce
Maciej Komorowski <mac.komorowski@gmail.com>
2018-04-19 09:19:47 +0200

    Pagination bar display refactor

--- src/main/java/com/example/blockchaininfo/configuration/WebConfig.java
+++ src/main/java/com/example/blockchaininfo/configuration/WebConfig.java
@@ -3,20 +3,19 @@ package com.example.blockchaininfo.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig{
 
-    @Override
+   // @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
         resolver.setFallbackPageable(PageRequest.of(0, 20));
         argumentResolvers.add(resolver);
-        this.addArgumentResolvers(argumentResolvers);
+        //super.addArgumentResolvers(argumentResolvers);
     }
-
 }
