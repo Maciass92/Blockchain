7f6236ae53b5a66ac52370f520139c1effb40457
Maciej Komorowski <mac.komorowski@gmail.com>
2018-04-19 12:07:20 +0200

    Code refactor.

--- src/main/java/com/example/blockchaininfo/configuration/WebConfig.java
+++ src/main/java/com/example/blockchaininfo/configuration/WebConfig.java
@@ -4,18 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
 @Configuration
 public class WebConfig{
 
-   // @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
         PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
         resolver.setFallbackPageable(PageRequest.of(0, 20));
         argumentResolvers.add(resolver);
-        //super.addArgumentResolvers(argumentResolvers);
     }
 }
