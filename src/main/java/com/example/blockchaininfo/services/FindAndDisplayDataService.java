1660893e0d93899bf3aed255c2feadfbc968382b
Maciej Komorowski <mac.komorowski@gmail.com>
2018-04-27 12:29:44 +0200

    Refactored content refresh to work client side only.

--- src/main/java/com/example/blockchaininfo/services/FindAndDisplayDataService.java
+++ src/main/java/com/example/blockchaininfo/services/FindAndDisplayDataService.java
@@ -6,11 +6,14 @@ import com.example.blockchaininfo.repositories.NetworkHashrateRepository;
 import com.example.blockchaininfo.repositories.PoolHashrateRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -48,7 +51,7 @@ public class FindAndDisplayDataService {
 
     public String formatDate(OffsetDateTime dateTime){
 
-        return DateTimeFormatter.ofPattern("dd.MM.yyyy - HH:mm:ss:SSS").format(dateTime);
+        return DateTimeFormatter.ofPattern("yyyy.MM.dd - HH:mm:ss:SSS").format(dateTime);
     }
 
     public double formatHashrate(double hashrate){
